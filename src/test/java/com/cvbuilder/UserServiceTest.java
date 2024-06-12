0141c43e2751ff5610969cb44068230c5f257933
Luana Machado <luanavimachado@gmail.com>
2024-06-12 16:46:41 -0300

    test/user: adding junit dependency to pom.xml

---
+++ src/test/java/com/cvbuilder/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.cvbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Test
+    void createUser() {
+
+    }
+
+}
